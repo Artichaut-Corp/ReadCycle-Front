75f3373464c3e64262937e859ed9eca711126cf2
Snowfox-1st <mart@sbertin.fr>
2023-08-15 20:07:37 +0200

    tailwind/svelte add and my first component with svelte

---
+++ orbital-orbit/src/components/ButtonPlusSvelte.svelte
@@ -0,0 +1,10 @@
+<script>
+    let count =0;
+    function increment() {
+        count+=1;
+    }
+</script>
+
+<button on:click={increment} class="btn btn-primary">Ce bouton a été cliqué {count} fois</button>
+
+
