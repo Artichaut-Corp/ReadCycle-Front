03d97ca841d04519a65a30ec2bde8dafb6c4a9b1
Artichaut <artichaut432@gmail.com>
2023-08-16 11:26:50 +0200

    Header componet in svelte

---
+++ orbital-orbit/src/components/ButtonPlusSolid.tsx
@@ -0,0 +1,12 @@
+import { createSignal } from 'solid-js';
+
+export default function BoutonPlusSolid() {
+  const [count, setCount] = createSignal(0);
+  const add = () => setCount(count() + 1);
+
+  return (
+      <>
+          <button class="btn btn-primary" onClick={add}>Ce bouton a été cliqué {count()} fois</button>
+        </>
+        );
+  }
