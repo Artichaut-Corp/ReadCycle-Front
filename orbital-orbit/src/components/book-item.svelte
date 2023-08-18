cb629797302aae79607382f6aba1e4c46a14cf6b
Snowfox-1st <mart@sbertin.fr>
2023-08-18 15:36:21 +0200

    Update books/colors

---
+++ orbital-orbit/src/components/book-item.svelte
@@ -0,0 +1,17 @@
+<script>
+
+
+
+
+</script>
+
+<div class="card card-side bg-base-200 shadow-xl">
+    <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
+    <div class="card-body">
+      <h2 class="card-title">New movie is released!</h2>
+      <p>Click the button to watch on Jetflix app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Watch</button>
+      </div>
+    </div>
+  </div>
---
+++ orbital-orbit/src/components/books.svelte
@@ -0,0 +1,21 @@
+<script>
+
+import BookItem from "./book-item.svelte";
+
+    const books = [
+        {
+            "title": "Livre Test",
+            "author": "FHPLOUF"
+        },
+        {
+            "title": "Livre Test 2",
+            "author": "kfpfk"
+        }
+    ]
+
+</script>
+<ul>
+    {#each books as book}
+        <BookItem>Titre: {book.title} Auteur: {book.author}</BookItem>
+    {/each}
+</ul>
