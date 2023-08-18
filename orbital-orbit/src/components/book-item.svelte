99d45f9faee4c74b45e266abb354d39cc3f4196e
Snowfox-1st <mart@sbertin.fr>
2023-08-18 17:25:14 +0200

    open /close summary button

--- orbital-orbit/src/components/book-item.svelte
+++ orbital-orbit/src/components/book-item.svelte
@@ -1,17 +1,30 @@
 <script>
-
-
-
-
+    import { writable } from "svelte/store";
+    
+    let condition = writable(false);
 </script>
+   
 
 <div class="card card-side bg-base-200 shadow-xl">
-    <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
     <div class="card-body">
-      <h2 class="card-title">New movie is released!</h2>
-      <p>Click the button to watch on Jetflix app.</p>
+      <p class="card-title">Title:</p>
+      <p class="card title">Author:</p>
+      <p>Genre:</p>
+      <p>Editor:</p>
+      <p>ISBN:</p>
+      {#if $condition }
+        <p>sommaire</p>
+      
       <div class="card-actions justify-end">
-        <button class="btn btn-primary">Watch</button>
+        <button class="btn btn-primary" on:click={() =>  condition.set(false) }>Hide</button>
+        
       </div>
+      {:else}
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary" on:click={() =>  condition.set(true) }>View summary</button>
+        
+      </div>
+      {/if}
+      
     </div>
   </div>
