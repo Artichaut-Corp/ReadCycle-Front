bd12685a1147ae5392210ff5b95ca5293acf8138
Snowfox-1st <mart@sbertin.fr>
2023-08-24 16:00:58 +0200

    Books
    Still a CHAD

--- orbital-orbit/src/components/book-item.svelte
+++ orbital-orbit/src/components/book-item.svelte
@@ -1,19 +1,24 @@
 <script>
     import { writable } from "svelte/store";
-    
+    export let Title
+    export let Author
+    export let Genre
+    export let Editor
+    export let ISBN
+    export let Summary
     let condition = writable(false);
 </script>
    
 
 <div class="card card-side bg-base-200 shadow-xl mx-5 my-5">
     <div class="card-body">
-      <p class="card-title">Title:</p>
-      <p class="card title">Author:</p>
-      <p>Genre:</p>
-      <p>Editor:</p>
-      <p>ISBN:</p>
+      <p class="card-title">Title : {Title}</p>
+      <p class="card title">Author : {Author}</p>
+      <p>Genre : {Genre}</p>
+      <p>Editor : {Editor}</p>
+      <p>ISBN : {ISBN}</p>
       {#if $condition }
-        <p>sommaire</p>
+        <p>Summary : {@html Summary}</p>
       
       <div class="card-actions justify-end">
         <button class="btn btn-primary" on:click={() =>  condition.set(false) }>Hide summary</button>
--- orbital-orbit/src/components/books.svelte
+++ orbital-orbit/src/components/books.svelte
@@ -1,22 +1,12 @@
 <script>
-
 import BookItem from "./book-item.svelte";
 
+export let resultat
 
-    const books = [
-        {
-            "title": "Livre Test",
-            "author": "FHPLOUF"
-        },
-        {
-            "title": "Livre Test 2",
-            "author": "kfpfk"
-        }
-    ]
-
-</script>
+</script>                     
+    
 <ul>
-    {#each books as book}
-        <BookItem>Titre: {book.title} Auteur: {book.author}</BookItem>
+    {#each resultat.items as book}
+        <BookItem Title={book.title} Author={book.author} Genre={book.genre_id} Editor={book.editor} ISBN={book.isbn} Summary={book.summary}/>
     {/each}
 </ul>
