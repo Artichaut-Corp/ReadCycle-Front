8f60eaaa8f9d8fc8a60ee105af022cbf9a9b5af4
Artichaut <artichaut432@gmail.com>
2023-09-21 20:24:38 +0200

    updated for ci

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,7 @@
 <script>
-  import  pb from '../pocketbase.ts'
 
   export let userData;
   // N'est meme pas exécutée
-  const logout = async () => {
-    pb.authStore.clear()
-  }
-
 </script>
 
 <navbar> 
@@ -34,6 +29,7 @@
             <label tabindex="0" class="btn btn-ghost rounded-btn">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg> 
             </label>
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
             <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-200 rounded-box w-52 mt-4">
               {#if userData.isValid}
                 <li><a class="link" href="/profile">Profile</a></li>
