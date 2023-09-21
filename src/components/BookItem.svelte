2e0cce999b32a99d6a4d725f9829bb97a414d0b5
Artichaut <artichaut432@gmail.com>
2023-09-21 19:19:21 +0200

    expand books request in order to get real names and not just id's

--- src/components/BookItem.svelte
+++ src/components/BookItem.svelte
@@ -13,23 +13,32 @@
 
 <div class="card card-side bg-base-200 shadow-xl mx-5 my-5 min-w-fit max-w-xl">
     <div class="card-body">
-      <h1 class="card-title link"><a href="/books/{Id}">Title: {Title}</a></h1>
-      <p><span class="text-neutral-content text-lg font-bold">Author: </span>{Author}</p>
-      <p><span class="text-neutral-content text-lg font-bold">Genre: </span>{Genre}</p>
-      <p><span class="text-neutral-content text-lg font-bold">Editor: </span>{Editor}</p>
+      <h1 class="card-title link"><a href="/books/{Id}">Titre: {Title}</a></h1>
+      <p><span class="text-neutral-content text-lg font-bold">Auteur: </span><a href="/authors/{Author.id}" class="link">{Author.first_name}, {Author.last_name}</a></p>
+      <p>
+        <span class="text-neutral-content text-lg font-bold">Genres: </span>
+        {#each Genre as genre, index}
+          {#if index == Genre.length -1 }
+            {genre.name}
+          {:else}
+            {genre.name + ", "}
+          {/if}
+        {/each}
+      </p>
+      <p><span class="text-neutral-content text-lg font-bold">Editeur: </span><a href="{Editor}" class="link">{Editor}</a></p>
       <p><span class="text-neutral-content text-lg font-bold">ISBN: </span>{ISBN}</p>
 
       {#if $condition }
-        <span class="text-neutral-content text-lg font-bold">Summary: </span>
+        <span class="text-neutral-content text-lg font-bold">Sommaire: </span>
         <div class="overflow-y-scroll h-52 border-solid border-base-100 outline-4 border-4 rounded-md">
           <p>{@html Summary}</p>
         </div>
         <div class="card-actions justify-end ">
-          <button class="btn btn-primary" on:click={() =>  condition.set(false) }>Hide summary</button>
+          <button class="btn btn-primary" on:click={() =>  condition.set(false) }>Cacher le sommaire</button>
         </div>
       {:else}
       <div class="card-actions justify-end">
-        <button class="btn btn-primary" on:click={() =>  condition.set(true) }>View summary</button>
+        <button class="btn btn-primary" on:click={() =>  condition.set(true) }>Voir le sommaire</button>
       </div>
       {/if}
     </div>
