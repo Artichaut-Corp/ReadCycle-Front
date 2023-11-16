01421509d69ab538267b4601cabacf211a7cfa9b
Artichaut <artichaut432@gmail.com>
2023-11-16 22:02:36 +0100

    removing sharp because uncompatible with deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import solidJs from "@astrojs/solid-js";
 
-import deno from "@astrojs/deno";
+import  deno  from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), svelte(), solidJs()],
-  adapter: deno()
+  adapter: deno(),
+  image: {
+    service: squooshImageService(),
+  },
 });
